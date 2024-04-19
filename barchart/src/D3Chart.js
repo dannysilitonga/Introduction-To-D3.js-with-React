5e320ee23fe5c2d494c886d344b8ce60b8ee5bb4
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2024-04-19 15:08:55 +0000

    created sentiment viz

--- barchart/src/D3Chart.js
+++ barchart/src/D3Chart.js
@@ -1,8 +1,8 @@
 import * as d3 from 'd3'
 
-const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 }
-const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
-const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+const MARGIN = { TOP: 30, BOTTOM: 300, LEFT: 90, RIGHT: 30 }
+const WIDTH = 860 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 800 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
 	constructor(element) {
@@ -33,11 +33,13 @@ export default class D3Chart {
 		vis.yAxisGroup = vis.svg.append("g")
 
 		Promise.all([
-			d3.json("https://udemy-react-d3.firebaseio.com/tallest_men.json"),
-			d3.json("https://udemy-react-d3.firebaseio.com/tallest_women.json")
+			d3.json("https://sentimentviz-default-rtdb.firebaseio.com/top_videos_24_15_03.json"),
+			d3.json("https://sentimentviz-default-rtdb.firebaseio.com/top_videos_24_15_04.json")
 		]).then((datasets) => {
-			vis.menData = datasets[0]
-			vis.womenData = datasets[1]
+			
+			vis.menData = restructureData(datasets[0]).slice(0, 20)
+			console.log("mensData", vis.menData)
+			vis.womenData = restructureData(datasets[1]).slice(0,20)
 			vis.update("men")
 		})
 	}
@@ -48,52 +50,66 @@ export default class D3Chart {
 		vis.data = (gender == "men") ? vis.menData : vis.womenData;
 		vis.xLabel.text(`The world's tallest ${gender}`)
 
-		const y = d3.scaleLinear()
-			.domain([
-				d3.min(vis.data, d => d.height) * 0.95, 
-				d3.max(vis.data, d =>  d.height)
-			])
-			.range([HEIGHT, 0])
-
 		const x = d3.scaleBand()
-			.domain(vis.data.map(d => d.name))
-			.range([0, WIDTH])
-			.padding(0.4)
+      .range([0, WIDTH])
+      .domain(vis.data.map((d) => d.videoTitle))
+      .padding(0.5)
+    const xAxisCall = d3.axisBottom(x)
+    vis.xAxisGroup.transition().duration(500).call(xAxisCall)
+      .selectAll("text")
+      .style("font-size", "8px")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end")
+  
+    const y = d3.scaleLinear()
+        .domain([
+          d3.min(vis.data, d => d.videoCount) *0.95, 
+          d3.max(vis.data, d => d.videoCount)
+        ])
+        .range([HEIGHT, 0])
+  
+    const yAxisCall = d3.axisLeft(y)
+    vis.yAxisGroup.transition().duration(500).call(yAxisCall)
+    console.log("hello again")
 
-		const xAxisCall = d3.axisBottom(x)
-		vis.xAxisGroup.transition().duration(500).call(xAxisCall)
+    // DATA JOIN
+    const rects = vis.svg.selectAll("rect")
+      .data(vis.data)
 
-		const yAxisCall = d3.axisLeft(y)
-		vis.yAxisGroup.transition().duration(500).call(yAxisCall)
+    // EXIT 
+    rects.exit()
+        .transition().duration(500)
+        .attr("height", 0)
+        .attr("y", HEIGHT)
+        .remove()
 
-		// DATA JOIN
-		const rects = vis.svg.selectAll("rect")
-			.data(vis.data)
+    // UPDATE 
+    rects.transition().duration(500)
+      .attr("x", d => x(d.videoTitle))
+      .attr("y", d => y(d.videoCount))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.videoCount))
 
-		// EXIT
-		rects.exit()
-			.transition().duration(500)
-				.attr("height", 0)
-				.attr("y", HEIGHT)
-				.remove()
-
-		// UPDATE
-		rects.transition().duration(500)
-			.attr("x", d => x(d.name))
-			.attr("y", d => y(d.height))
-			.attr("width", x.bandwidth)
-			.attr("height", d => HEIGHT - y(d.height))
-
-		// ENTER
-		rects.enter().append("rect")
-			.attr("x", d => x(d.name))
-			.attr("width", x.bandwidth)
-			.attr("fill", "grey")
-			.attr("y", HEIGHT)
-			.transition().duration(500)
-				.attr("height", d => HEIGHT - y(d.height))
-				.attr("y", d => y(d.height))
-
-			console.log(rects)
+    // ENTER
+    rects.enter().append("rect")
+      .attr("x", d => x(d.videoTitle))
+      .attr("width", x.bandwidth)
+      .attr("fill", "#5f0f40")
+      .attr("y", HEIGHT)
+      .transition().duration(500)
+        .attr("height", d => HEIGHT - y(d.videoCount))
+        .attr("y", d => y(d.videoCount))
 	}
+}
+
+function restructureData(raw_data) {
+	var USAVideoCount = [];
+	for(const videoID in raw_data['US']) {
+		USAVideoCount.push({
+			videoTitle: videoID,
+			videoCount: Number(raw_data['US'][videoID]['view_count'])
+		});
+	}
+	USAVideoCount = USAVideoCount.sort((a,b) => (a.videoCount < b.videoCount) ? 1: -1) 
+	return USAVideoCount
 }
