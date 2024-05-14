2a0a15d6094f6ba418b4a6d9c7820dd4ec8a18a0
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2024-05-14 02:06:43 +0000

    started working on scatterplot

--- barchart/src/D3Stacked.js
+++ barchart/src/D3Stacked.js
@@ -1,37 +1,47 @@
-import * as d3 from 'd3'
+import * as d3 from 'd3';
 
 const MARGIN = { TOP: 30, BOTTOM: 300, LEFT: 90, RIGHT: 30 }
 const WIDTH = 860 - MARGIN.LEFT - MARGIN.RIGHT;
 const HEIGHT = 800 - MARGIN.TOP - MARGIN.BOTTOM;
+const marginTop = 10;
+  const marginRight = 10;
+  const marginBottom = 20;
+  const marginLeft = 40;
 
 export default class D3Chart {
 	constructor(element) {
 		const vis = this
 
-		vis.svg = d3.select(element)
-			.append("svg")
-				.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-				.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-			.append("g")
-				.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+		//vis.svg = d3.select(element)
+		//	.append("svg")
+		//		.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+		//		.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+		//	.append("g")
+		//		.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
-		vis.xLabel = vis.svg.append("text")
-			.attr("x", WIDTH / 2)
-			.attr("y", HEIGHT + 80)
-			.attr("text-anchor", "middle")
-			.style("font-size", "19px")
+		//vis.svg = d3.create("svg")
+		//		.attr("width", WIDTH)
+		//		.attr("height", HEIGHT)
+		//	.attr("viewBox", [0,0, WIDTH,HEIGHT])
+		//	.attr("style", "max-width: 100%, height: auto;");
 
-		vis.svg.append("text")
-			.attr("x", -(HEIGHT / 2))
-			.attr("y", -75)
-			.attr("text-anchor", "middle")
-			.text("Number of Views")
-			.attr("transform", "rotate(-90)")
+		//vis.xLabel = vis.svg.append("text")
+		//	.attr("x", WIDTH / 2)
+		//	.attr("y", HEIGHT + 80)
+		//	.attr("text-anchor", "middle")
+		//	.style("font-size", "19px")
 
-		vis.xAxisGroup = vis.svg.append("g")
-			.attr("transform", `translate(0, ${HEIGHT})`)
+		//vis.svg.append("text")
+		//	.attr("x", -(HEIGHT / 2))
+		//	.attr("y", -75)
+		//	.attr("text-anchor", "middle")
+		//	.text("Number of Views")
+		//	.attr("transform", "rotate(-90)")
 
-		vis.yAxisGroup = vis.svg.append("g")
+		//vis.xAxisGroup = vis.svg.append("g")
+	//		.attr("transform", `translate(0, ${HEIGHT})`)
+
+		//vis.yAxisGroup = vis.svg.append("g")
 
 		Promise.all([
 			d3.json("https://sentimentviz-default-rtdb.firebaseio.com/sentiments_24_15_03.json"),
@@ -72,15 +82,16 @@ export default class D3Chart {
 			console.log("march 15 Data", march15DataTemp)
 
 			vis.march15Data = reorganizeData(march15DataTemp).slice(0, 20); // march15DataTemp.slice(0, 20); //
+			//vis.march15Data = reorganizeData(march15DataTemp.slice(0, 20); // march15DataTemp.slice(0, 20); //
 			vis.march22Data = reorganizeData(march22DataTemp).slice(0, 20); //march22DataTemp.slice(0, 20); //
 			vis.march29Data = reorganizeData(march29DataTemp).slice(0, 20); //march29DataTemp.slice(0, 20); //reorganizeData(march29DataTemp).slice(0, 20);
 			vis.april05Data = reorganizeData(april05DataTemp).slice(0, 20); //april05DataTemp.slice(0, 20); // reorganizeData(april05DataTemp).slice(0, 20);
 			vis.april15Data = reorganizeData(april15DataTemp).slice(0, 20); //april15DataTemp.slice(0, 20); //reorganizeData(april15DataTemp).slice(0, 20);
 
 			console.log("april15Data", vis.april15Data)
-			//console.log("Number of Positive Comments from first video is ", vis.march15Data.find(item => item.videoID === "0AW21jPu-Wc")['sentimentData'].find(item => item.analysis === "Positive")['count'])
-			//console.log("The categories of sentiments are ", vis.march15Data.find(item => item.videoID === "0AW21jPu-Wc")['sentimentData'])
-			//var dataExp = {};
+			console.log("Number of Positive Comments from first video is ", vis.march15Data.find(item => item.videoID === "0AW21jPu-Wc")['sentimentData'].find(item => item.analysis === "Positive")['count'])
+			console.log("The categories of sentiments are ", vis.march15Data.find(item => item.videoID === "0AW21jPu-Wc")['sentimentData'])
+			var dataExp = {};
 
 
 			//var dataExp.map(( d = )
@@ -88,10 +99,10 @@ export default class D3Chart {
 			//} 
 			//vis.march15Data.flatMap(videoID => [videoID['sentimentData'].find(item => item.analysis =="Negative")['count']])
 
-			//console.log("experimental reorg", vis.march15Data.flatMap( a => [ a.sentimentData.map(b => [b.analysis, b.count, a.videoID] )]));//.map(c =>c.Negative)]));  //concat(a)] )); //[ a.sentimentData.concat(a)] ));
-			//console.log("experimental reorg2", vis.march15Data[0]['sentimentData'].find(item => item.analysis =="Negative")['count'])
-			//console.log("experimental reorg3", vis.march15Data.forEach(videoID => [ videoID.negativeSentiment = videoID['sentimentData'].find(item => item.analysis =="Negative")['count']] ))
-			//console.log("experimental reorg4", vis.march15Data.flatMap(videoID => ({NegativeCount: [videoID['sentimentData'].find(item => item.analysis =="Negative")['count']]})))
+			console.log("experimental reorg", vis.march15Data.flatMap( a => [ a.sentimentData.map(b => [b.analysis, b.count, a.videoID] )]));//.map(c =>c.Negative)]));  //concat(a)] )); //[ a.sentimentData.concat(a)] ));
+			console.log("experimental reorg2", vis.march15Data[0]['sentimentData'].find(item => item.analysis =="Negative")['count'])
+			console.log("experimental reorg3", vis.march15Data.forEach(videoID => [ videoID.negativeSentiment = videoID['sentimentData'].find(item => item.analysis =="Negative")['count']] ))
+			console.log("experimental reorg4", vis.march15Data.flatMap(videoID => ({NegativeCount: [videoID['sentimentData'].find(item => item.analysis =="Negative")['count']]})))
 			//console.log("experimental reorg5", dataExp)
 			//console.log("experimental reorg5", vis.march15Data.flatMap( a => [ a.sentimentData.map(b => [{b.analysis: b.count}, a.videoID] )]))
 
@@ -127,20 +138,79 @@ export default class D3Chart {
 			//console.log('expiremental reorg5', DataExp2)
 
 
-			var newData = [];
-			for(const videoID in vis.march15Data) {
-				newData.push({
-				//videoTitle: videoID,
-				negativeSentiment: vis.march15Data.map(videoID => [videoID['sentimentData'].find(item => item.analysis =="Negative")['count']])
-				});
-			}
-			console.log("the new exp reorg4", newData)
-			
-		
+			//var newData = [];
+			//for(const videoID in vis.march15Data) {
+			//	newData.push({
+			//	//videoTitle: videoID,
+			//	negativeSentiment: vis.march15Data.map(videoID => [videoID['sentimentData'].find(item => item.analysis =="Negative")['count']])
+			//	});
+			//}
+			//console.log("the new exp reorg4", newData)
+			const data = vis.march15Data;
+			 // Determine the series that need to be stacked.
+			const series = d3.stack()
+			.keys(d3.union(data.map(d => d.analysis))) // distinct series keys, in input order
+			.value(([, D], key) => D.get(key).count) // get value for each series key and stack
+			(d3.index(data, d => d.videoID, d => d.analysis));
 
-			vis.update("march15")
-		})
-	}
+			  //Prepare the scales for positional and color encodings.
+			const x = d3.scaleBand()
+			.domain(d3.groupSort(data, D => -d3.sum(D, d => d.count), d => d.videoID))
+			.range([marginLeft, WIDTH - marginRight])
+			.padding(0.1);
+
+			const y = d3.scaleLinear()
+			.domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+			.rangeRound([HEIGHT - marginBottom, marginTop]);
+
+			const color = d3.scaleOrdinal()
+			.domain(series.map(d =>d.key))
+			.range(d3.schemeSpectral[series.length]) 
+			.unknown("#cc");
+
+			//A function to format the value in the tooltip.
+			const formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en")
+
+			//Create the SVG container
+			vis.svg = d3.create("svg")
+			.attr("width", WIDTH)
+			.attr("height", HEIGHT)
+			.attr("viewBox", [0,0, WIDTH, HEIGHT])
+			.attr("style", "max-width: 100%, height: auto;");
+
+			
+			
+			//Append a group for each series, and a rect for each element in the series.
+			vis.svg.append("g")
+			.selectAll()
+			.data(series)
+			.join("g")
+			.attr("fill", d => color(d.key))
+			.selectAll("rect")
+			.data(D => D.map(d => (d.key = D.key, d)))
+			.join("rect")
+				.attr("x", d => x(d.data[0]))
+				.attr("y", d => y(d[1]))
+				.attr("height", d => y(d[0]) - y(d[1]))
+				.attr("width", x.bandwidth())
+			.append("title")
+			.text(d => `${d.data[0]} ${d.key}\n${formatValue(d.data[1].get(d.key).count)}`);
+
+
+			 //Append the horizontal axis
+			vis.svg.append("g")
+			.attr("transform", `translate(0,${HEIGHT - marginBottom})`) 
+			.call(d3.axisBottom(x).tickSizeOuter(0))
+			.call(g => g.selectAll('.domain').remove());
+
+			//Append the vertical axis
+			vis.svg.append("g")
+			.attr("transform", `translate($marginLeft},0)`)
+			.call(g => g.selectAll(".domain").remove());
+
+		// vis.update("march15")
+					})
+				}
 
 	update(date) {
 		const vis = this
@@ -161,9 +231,9 @@ export default class D3Chart {
 		//console.log("converted date month", converted_month_date)
 
 
-		vis.xLabel.text(`Top Trending Videos on ${converted_month_date}`)
+		//vis.xLabel.text(`Top Trending Videos on ${converted_month_date}`)
 
-		console.log("the video data", vis.data)
+		//console.log("the video data", vis.data)
 
 		const sentimentCategories = ['Negative', 'Neutral', 'Positive']
 		console.log("sentimentCategories", sentimentCategories)
@@ -171,45 +241,70 @@ export default class D3Chart {
 		// Determine the series that need to be stacked.  
 		//const series = d3.stack()
 	//		.keys(d3.union(vis.data.map(d=>d)))
-		console.log("the new data", vis.data.map(d=>d.sentimentData).map(d=>d.analysis))
+		//console.log("the new data", vis.data.map(d=>d.sentimentData).map(d=>d.analysis))
 
 		const x = d3.scaleBand()
       		.range([0, WIDTH])
-      		.domain(vis.data.map((d) => d.videoID))
+      		//.domain(vis.data.map((d) => d.videoID))
+			.domain(d3.groupSort(vis.data, D => -d3.sum(D, d => d.count), d => d.videoID))
 			.padding(0.5)
     	
-		const xAxisCall = d3.axisBottom(x)
+		// const xAxisCall = d3.axisBottom(x)
     
-		vis.xAxisGroup.transition().duration(500).call(xAxisCall)
-      		.selectAll("text")
-      		.style("font-size", "8px")
-      		.attr("transform", "translate(-10,0)rotate(-45)")
-      		.style("text-anchor", "end")
+		//vis.xAxisGroup.transition().duration(500).call(xAxisCall)
+      	//	.selectAll("text")
+      	//	.style("font-size", "8px")
+      	//	.attr("transform", "translate(-10,0)rotate(-45)")
+      	//	.style("text-anchor", "end")
   
-    	const y = d3.scaleLinear()
-        	.domain([
-          	d3.min(vis.data, d => d.videoCount) *0.95, 
-          	d3.max(vis.data, d => d.videoCount)
-        	])
-        	.range([HEIGHT, 0])
+    	//const y = d3.scaleLinear()
+        //	.domain([
+        // 	d3.min(vis.data, d => d.videoCount) *0.95, 
+        //  	d3.max(vis.data, d => d.videoCount)
+        //	])
+        //	.range([HEIGHT, 0])
+
+		//console.log(d3);	
+
+		const series = d3.stack()
+			.keys(d3.union(vis.data.map(d => d.analysis))) // distinct series keys, in input order
+			.value(([, D], key) => D.get(key).count) // get value for each series key adn stack
+			(d3.index(vis.data, d =>d.videoID, d => d.analysis));
+
+		const y = d3.scaleLinear()
+			.domain([0, d3.max(series, d => d3.max(d, d => d[1]))]) 
+			.rangeRound([HEIGHT, 0]);
   
-    	const yAxisCall = d3.axisLeft(y)
-    	vis.yAxisGroup.transition().duration(500).call(yAxisCall)
-    	console.log("hello again")
+    	//const yAxisCall = d3.axisLeft(y)
+    	//vis.yAxisGroup.transition().duration(500).call(yAxisCall)
+    	//console.log("hello again")
 		
 		//color palette = one color per subgroup
 		const color = d3.scaleOrdinal()
-			.domain(sentimentCategories)
-			.range(['#e41a1c','#377eb8','#4daf4a'])
+			.domain(series.map(d => d.key))
+			.range(d3.schemeSpectral[series.length])
+			.unknown("#ccc"); 
 			
+		const formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en")
 
     	// DATA JOIN
-    	const rects = vis.svg.selectAll("rect")
-      		.data(vis.data)
+    	const rects =    //Append a group for each series, and a rect for each element in the series.
+		vis.svg.append("g")
+		  .selectAll()
+		  .data(series)
+		  .join("g")
+		  .attr("fill", d => color(d.key))
+		  .selectAll("rect")
+		  .data(D => D.map(d => (d.key = D.key, d)))
+		  .join("rect")
+			.attr("x", d => x(d.data[0]))
+			.attr("y", d => y(d[1]))
+			.attr("height", d => y(d[0]) - y(d[1]))
+			.attr("width", x.bandwidth())
+		  .append("title")
+		  .text(d => `${d.data[0]} ${d.key}\n${formatValue(d.data[1].get(d.key).count)}`);
 
-		const stackData = d3.stack()
-			.keys(sentimentCategories)
-			(vis.data)
+		
 
     	// EXIT 
     	rects.exit()
